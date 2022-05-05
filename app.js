556306826fef62a89fde4f5359d6730665b29d9c
Paulo Araújo <90206752+86Paulo@users.noreply.github.com>
2022-05-05 22:43:41 +0100

    Add event listeners

--- app.js
+++ app.js
@@ -2,9 +2,16 @@
 const modalBtn = document.querySelector(".modal-btn");
 const modal = document.querySelector(".modal-overlay");
 const closeBtn = document.querySelector(".close-btn");
-// listen for click events on modal-btn and close-btn
-modalBtn.addEventListener("click", function(){
+
+// listen for click events on modal-btn 
+modalBtn.addEventListener("click", function () {
     // when user clicks modal-btn add .open-modal to modal-overlay
-    
+    modal.classList.add("open-modal");
 })
+
+// listen for click events on close-btn
+closeBtn.addEventListener("click", function () {
 // when user clicks close-btn remove .open-modal from modal-overlay
+    modal.classList.remove("open-modal");
+})
+
